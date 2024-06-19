925f6c056f54959bacdb90627a6adf89add9ebcb
Arnaud Sene <arnaud.sene@pm.me>
2024-06-19 10:00:35 -0400

    feat: cleanup config - Config Ready

--- utils/index.ts
+++ utils/index.ts
@@ -1,7 +1,5 @@
 import { simulateContract, writeContract,GetBlockNumberErrorType, readContract } from 'wagmi/actions'
 import { config } from '../config';
-import { allfeat } from '../config/network';
-import { sepolia } from 'viem/chains';
 import { BaseError, ContractFunctionRevertedError } from 'viem';
 
 
@@ -9,22 +7,14 @@ export const writeContractByFunctionName = async (
     functionName: string,
     abi: any[],
     contractAddress: `0x${string}`,
-    network: string,
     ...args: `0x${string}`[] | any[]
   ): Promise<`0x${string}`> => {
-  
-    let _chainId = allfeat.id
-      
-    if (network === "sepolia") {
-        _chainId = sepolia.id
-    }
 
     try {
         const result_simu = await simulateContract(config, {
             abi: abi,
             address: contractAddress,
             functionName: functionName,
-            // chainId: _chainId, 
             args: args,
         })
 
@@ -32,16 +22,14 @@ export const writeContractByFunctionName = async (
             abi: abi,
             address: contractAddress,
             functionName: functionName,
-            // chainId: _chainId,
             args: args,
         })
           
       return result;
     } catch (e) {
-		const error = e as GetBlockNumberErrorType
+		  const error = e as GetBlockNumberErrorType
       console.log("utils.index.writeContractByFunctionName error :", error)
-      // throw formattedError(err);
-      return "0x0"
+      throw formattedError(error);
     }
   }
 
@@ -51,30 +39,22 @@ export const readContractByFunctionName = async <T>(
     abi: any,
     contractAddress: `0x${string}`,
     address: `0x${string}`,
-    network: string,
     ...args: `0x${string}`[] | any[]
   ): Promise<T> => {
-  
-      let _chainId = allfeat.id
-      
-      if (network === "sepolia") {
-		_chainId = sepolia.id
-      }
 
-  
     try {
       const data: Promise<T> | unknown = await readContract(config, {
         address: contractAddress,
         abi: abi,
         functionName: functionName,
-        // chainId: _chainId,
         account: address,
         args: args,
       });
   
       return data as T;
-    } catch (err) {
-      throw formattedError(err);
+    } catch (e) {
+      const error = e as GetBlockNumberErrorType
+      throw formattedError(error);
     }
   }
 
